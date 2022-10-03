81fc59b18044165d1cdab3cd7726fcc591ce6d0e
ductai <ductai200920@gmail.com>
2022-10-03 14:11:41 +0700

    yolo

--- base/urls.py
+++ base/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('upimage/<str:pk>', views.upImage, name = 'UpLoadImg'),
     path('upimg/<str:pk>', views.upimg, name = 'upimg'),
     path('predict/<str:pk>', views.predict, name = 'predict'),
+    path('yolo_deepsort/', views.yolov5_deepsort, name = 'yolov5_deepsort'),
+    path('video_feed/', views.video_feed, name='video_feed')
 ]
